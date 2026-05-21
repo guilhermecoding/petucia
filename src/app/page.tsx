71cb4db979221f5cdbad6c1a82383bbe9c3872dd
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2026-05-21 17:53:25 -0300

    :sparkles: Add Petucia avatar SVG and update homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flez flex-row">
+      {/* Dados */}
+      <div>
+        <h1>[frases]</h1>
+        <span>Umidade do solo: [umidade]</span>
+      </div>
+
+      {/* Avatar da Petucia */}
+      <div>
+        <Image
+          src="/avatar-petucia.svg"
+          alt="Avatar da Petucia"
+          width={100}
+          height={100}
+          loading="eager"
+          className="w-auto h-[90vh] object-cover pointer-events-none"
+        />
+      </div>
     </div>
   );
 }
